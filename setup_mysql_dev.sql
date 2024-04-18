c68ca530fcfccec01ad91acbf5d799804536f7e4
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-18 15:14:55 +0100

    feat: add script that setup MYSQL server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+--- This srcipt setup MYSQL server for this project
+
+--- Create hbnb_dev_db database
+CREATE DATABASE IF NOT EXISTs hbnb_dev_db;
+
+--- Create new user hbnb_dev
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+
+--- Grant hbnb_dev user all privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"locahost";
+
+--- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* 
+TO "hbnb_dev"@"localhost";
+
+--- Flush the privileges into MYSQL server
+FLUSH PRIVILEGES;
