4729b2f77e3323b19eecc085a2c962b06aab9e3d
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-18 18:17:05 +0100

    fix: comment typo error and db privilege typo

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,16 @@
---- This srcipt setup MYSQL server for this project
+-- This srcipt setup MYSQL server for this project
 
---- Create hbnb_test_db database
+-- Create hbnb_test_db database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---- Create new user hbnb_dev
+-- Create new user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---- Grant hbnb_dev user all privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'locahost';
+-- Grant hbnb_dev user all privileges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'locahost';
 
---- Grant SELECT privilege on performance_schema
+-- Grant SELECT privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
---- Flush the privileges into MYSQL server
+-- Flush the privileges into MYSQL server
 FLUSH PRIVILEGES;
