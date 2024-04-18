80ce813e9e0263ead7158cbd749bf8160f1f3eb8
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-18 18:21:03 +0100

    fix: comment typo and change " to '

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,16 @@
---- This srcipt setup MYSQL server for this project
+-- This srcipt setup MYSQL server for this project
 
---- Create hbnb_dev_db database
+-- Create hbnb_dev_db database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
---- Create new user hbnb_dev
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+-- Create new user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
---- Grant hbnb_dev user all privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"locahost";
+-- Grant hbnb_dev user all privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'locahost';
 
---- Grant SELECT privilege on performance_schema
-GRANT SELECT ON performance_schema.* 
-TO "hbnb_dev"@"localhost";
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
---- Flush the privileges into MYSQL server
+-- Flush the privileges into MYSQL server
 FLUSH PRIVILEGES;
