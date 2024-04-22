2c2d19d6c0bfbcffc26a2587b3bc33967e6b434f
Yahaya Azeez <azconcept2016@gmail.com>
2024-04-22 04:18:14 +0100

    feat: add declaratuve mapping to db

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class with declarative mapping to amenities table"""
+
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = ''
