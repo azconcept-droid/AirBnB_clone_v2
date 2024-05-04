22f675f364d5a707eb7ae5093270a7ef6ac2ec98
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-04 05:37:36 +0100

    clean up

---
+++ 100-clean_web_static.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module generates .tgz archive"""
+
+from fabric import *
+from sys import argv
+
+
+env.user = argv[2]
+
+env.sshkey = argv[1]
+
+env.hosts = ['18.204.16.142', '54.175.223.202']
+
+
+def do_clean(number=0):
+    """This function archive webstatic folder"""
+
+    if number is 0 or 1:
+        pass
+
+    try:
+        put(archive_path, "/tmp/")
+
+        archive_name = archive_path.partition('.')
+
+        arch_name = archive_name[0]
+
+        source = "/tmp/{}".format(archive_path)
+
+        destination = "/data/web_static/releases/".format(arch_name)
+
+        run("tar -xvf {} -C {}".format(source, destination))
+
+        run("rm {}".format(archive_path))
+        run("unlink /data/web_static/current")
+        run("link -s {} /data/web_static/current".format(destination))
+    except Exception:
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    do_deploy()
