eda22ab42593db69986a0c08d7616d4495333802
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-04 04:10:16 +0100

    add: code to archive webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
 """This module generates .tgz archive"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """This function archive webstatic folder"""
+
+    # Current date and time
+    archive = datetime.now().strftime("web_static_%Y%m%d%H%M%S.tgz")
+
+    # Create the versions directory
+    try:
+        local("mkdir -p versions")
+    except Exception:
+        pass
+
+    arch_path = 'versions/{}'.format(archive)
+
+    # Archive the file
+    result = local("tar -cvzf {} web_static".format(arch_path))
+
+    if result.succeeded:
+        return arch_path
+    
+    return None
