449f456c1a5f4503c3e51b45d4508c05933392f3
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-09 06:36:05 -0700

    add: deployment function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""This module generates .tgz archive"""
+"""Deployment module"""
 
-from fabric import *
-from sys import argv
+from fabric.api import run, put
 
-env.user = argv[2]
-
-env.sshkey = argv[1]
-
+env.user = 'ubuntu'
 env.hosts = ['18.204.16.142', '54.175.223.202']
 
 
 def do_deploy(archive_path):
-    """This function archive webstatic folder"""
+    """Distribute archive web_static to web servers"""
 
     if archive_path is None:
         return False
@@ -20,24 +16,23 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
 
-        archive_name = archive_path.partition('.')
+        archive_part = archive_path.partition('/')
 
-        arch_name = archive_name[0]
+        archive_name = archive_part[1]
 
         source = "/tmp/{}".format(archive_path)
 
-        destination = "/data/web_static/releases/".format(arch_name)
+        destination = "/data/web_static/releases/".format(archive_name)
 
+        run("mkdir -p /data/web_satic/releases")
         run("tar -xvf {} -C {}".format(source, destination))
 
         run("rm {}".format(archive_path))
-        run("unlink /data/web_static/current")
+        run("rm -rf /data/web_static/current")
         run("link -s {} /data/web_static/current".format(destination))
+
+        return True
     except Exception:
         return False
 
-    return True
-
-
-if __name__ == "__main__":
-    do_deploy()
+do_deploy()
