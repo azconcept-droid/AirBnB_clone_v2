6efcdcb4d59aaf8aff3c48e1d32e0c00247a9cd9
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-05-10 17:57:55 +0100

    refactor code :recycle:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Deployment module"""
 
-from fabric.api import run, put
+from fabric.api import *
+import os
 
 env.user = 'ubuntu'
 env.hosts = ['18.204.16.142', '54.175.223.202']
@@ -10,28 +11,27 @@ env.hosts = ['18.204.16.142', '54.175.223.202']
 def do_deploy(archive_path):
     """Distribute archive web_static to web servers"""
 
-    if archive_path is None:
+    if not os.path.exists(archive_path):
         return False
 
     try:
-        put(archive_path, "/tmp/")
+        put(archive_path, '/tmp/')
 
-        archive_part = archive_path.partition('/')
+        archive_name= archive_path.partition('/')[-1]
 
-        archive_name = archive_part[1]
+        source = "/tmp/{}".format(archive_name)
 
-        source = "/tmp/{}".format(archive_path)
+        # archive name without extention
+        archive_no_ext = archive_name.partition('.')[0]
 
-        destination = "/data/web_static/releases/".format(archive_name)
+        destination = "/data/web_static/releases/".format(archive_no_ext)
 
-        run("mkdir -p /data/web_satic/releases")
         run("tar -xvf {} -C {}".format(source, destination))
 
-        run("rm {}".format(archive_path))
+        run("rm {}".format(source))
         run("rm -rf /data/web_static/current")
-        run("link -s {} /data/web_static/current".format(destination))
+        run("ln -s {} /data/web_static/current".format(destination))
 
         return True
     except Exception:
         return False
-
