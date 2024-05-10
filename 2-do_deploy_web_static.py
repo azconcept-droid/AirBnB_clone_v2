ef4279863ac90d1272adea117b028c3517bb617d
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-10 16:24:55 -0700

    write clean code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,19 +17,33 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
 
-        archive_name= archive_path.partition('/')[-1]
+        archive_name = archive_path.partition('/')[-1]
 
+        # Source path
         source = "/tmp/{}".format(archive_name)
 
-        # archive name without extention
+        # Archive name without extention
         archive_no_ext = archive_name.partition('.')[0]
 
-        destination = "/data/web_static/releases/".format(archive_no_ext)
+        # Path to destination
+        destination = "/data/web_static/releases/{}" + archive_no_ext
 
+        # Create destination folder
+        run("mkdir -p {}".format(destination))
+
+        # Uncompress to destination/web_static folder
         run("tar -xvf {} -C {}".format(source, destination))
 
+        # Move to destination from destination/web_static
+        run("mv {}/web_static/* {}".format(destination, destination))
+
+        # Remove /tmp/archive_name
         run("rm {}".format(source))
-        run("rm -rf /data/web_static/current")
+
+        # Remove symbolic link if it exist
+        run("rm -rf /data/webstatic/current")
+
+        # Create symbolic link to new release
         run("ln -s {} /data/web_static/current".format(destination))
 
         return True
